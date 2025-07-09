994c27581db0e56baf3935d9b40c8ae47020af84
anish <galacticoanish@gmail.com>
2025-07-09 10:04:26 +0545

    react

--- 10minicontext/src/App.jsx
+++ 10minicontext/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import Login from './components/login'
 import Profile from './components/profile'
 import './App.css'
